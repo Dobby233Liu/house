3a00d2aba64caa3151c0e7ee9ccbe7d93e08608b
Liu-Wen-Yuan <15816141883@163.com>
2017-09-20 07:26:53 -0500

    Update laundry.py

--- house/laundry.py
+++ house/laundry.py
@@ -24,79 +24,12 @@ class LaundryPlugin(Plugin):
         super(LaundryPlugin, self).load(ctx)
         self.data = {'washer': {}, 'dryer': {}}
 
-    def enqueue(self, device):
-        status = self.data.get(device)
-        if not status:
-            return
-
-        delay = status['when'] - time.time()
-        if delay <= 0:
-            return
-
-        time.sleep(delay)
-
-        status = self.data.get(device)
-        if not status:
-            return
-
-        status['channel'].send_message('Ok {}, your laundry is {}!'.format(status['user'].mention, DATA[device]['done']))
-
+   
     @Plugin.command('help')
     def help(self, event):
-        """
-        Displays this message.
-        """
-        tbl = MessageTable()
-        tbl.set_header('Command', 'Desc')
-
-        for command in self.bot.commands:
-            print command.get_docstring().strip()
-            tbl.add(command.triggers[0], command.get_docstring().strip())
-
-        event.msg.reply(tbl.compile())
-
-    @Plugin.command('washer start', group='laundry', context={'device': 'washer'})
-    @Plugin.command('dryer start', group='laundry', context={'device': 'dryer'})
-    def start(self, event, device=None):
-        """
-        Indicates the {device} has been started with a new load.
-        """
-        status = self.data[device]
-
-        if status:
-            event.msg.reply(':warning: {} was already running, clearing previous status'.format(device))
-
-        status['when'] = time.time() + DATA[device]['time']
-        status['user'] = event.author
-        status['channel'] = event.channel
-        status['subs'] = []
-        status['task'] = self.spawn(self.enqueue, device)
-        event.msg.reply(':ok_hand: started {}'.format(device))
-
-    @Plugin.command('washer done', group='laundry', context={'device': 'washer'})
-    @Plugin.command('dryer done', group='laundry', context={'device': 'dryer'})
-    def done(self, event, device=None):
-        """
-        Indicates the {device} has been emptied completely, and is ready for a new load.
-        """
-        status = self.data.get(device)
-        if not status:
-            event.msg.reply('The {} wasnt running!'.format(device))
-            return
-
-        if not status['task'].ready():
-            status['task'].kill()
-
-        extra = ''
-        if status['subs']:
-            nxt = status['subs'].pop()
-            extra = ' Its your turn now {}'.format(nxt.mention)
-
-        event.msg.reply(':ok_hand: {} is all ready to go.{}'.format(device, extra))
-
-    @Plugin.command('washer need', group='laundry', context={'device': 'washer'})
-    @Plugin.command('dryer need', group='laundry', context={'device': 'dryer'})
-    def queue(self, event, device=None):
+        
+        
+        
         """
         Adds you to the queue of people waiting to use the {device}.
         """
@@ -111,13 +44,7 @@ class LaundryPlugin(Plugin):
     @Plugin.command('washer when', group='laundry', context={'device': 'washer'})
     @Plugin.command('dryer when', group='laundry', context={'device': 'dryer'})
     def when(self, event, device=None):
-        """
-        Displays when the {device} will be done.
-        """
-        status = self.data.get(device)
-        if not status:
-            event.msg.reply('The {} isn\t in use.'.format(device))
-            return
+        
 
         content = humanize.naturaltime(datetime.fromtimestamp(status['when']))
         event.msg.reply('The {} will be done in {}.'.format(device, content))
